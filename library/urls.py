8a8c40bd6a0f47918ebaa2491af40bcac9babe46
akshar1277 <bhalani.akshar1277@gmail.com>
2022-11-12 19:22:15 +0530

    change is urls

--- library/urls.py
+++ library/urls.py
@@ -4,9 +4,10 @@ from .views import ProjectViewSet,Project_secondViewSet
 
 router=routers.DefaultRouter()
 
-router.register(r'Project_2019-20',ProjectViewSet)
-router.register(r'Project_2020-21',Project_secondViewSet)
+router.register(r'Project_2019-2020',ProjectViewSet)
+router.register(r'Project_2020-2021',Project_secondViewSet)
 
 urlpatterns = [
     path('',include(router.urls)),
+    
 ]
